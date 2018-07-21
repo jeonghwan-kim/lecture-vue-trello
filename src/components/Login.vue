c4ca0ab3abad941d0a6517eadf027409a21caba2
Chris <ej88ej@gmail.com>
2018-07-21 19:13:03 +0900

    style login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
+  <div class="login">
     <h2>Log in to Trello</h2>
     <form @submit.prevent="onSubmit">
       <div>
         <label for="email">Email</label>
-        <input type="text" name="email" v-model="email" autofocus placeholder="e.g., test@test.com" />
+        <input class="form-control" type="text" name="email" v-model="email" autofocus placeholder="e.g., test@test.com" />
       </div>
       <div>
         <label for="password">Passwrod</label>
-        <input type="password" v-model="password" placeholder="123123" />
+        <input class="form-control" type="password" v-model="password" placeholder="123123" />
       </div>
-      <button type="submit" :disabled="invalidForm">Log In</button>
+      <button  class="btn" :class="{'btn-success': !invalidForm}" type="submit" :disabled="invalidForm">Log In</button>
     </form>
-    <p v-if="error">{{error}}</p>
+    <p class="error" v-if="error">{{error}}</p>
   </div>
 </template>
 
@@ -34,6 +34,10 @@ export default {
   created() {
     this.returnPath = this.$route.query.returnPath || '/'
   },
+  mounted() {
+    document.querySelector('body').style.backgroundColor = '#fff'
+    document.querySelector('.header').style.backgroundColor = '#026aa7'
+  },
   methods: {
     onSubmit() {
       const {email, password} = this
@@ -48,3 +52,13 @@ export default {
   }
 }
 </script>
+
+<style>
+.login {
+  width: 400px;
+  margin: 0 auto; 
+}
+.error {
+  color: #f00;
+}
+</style>

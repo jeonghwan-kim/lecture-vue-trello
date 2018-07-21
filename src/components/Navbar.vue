657f9f46e90d14b2d0eeed5ddf03564c94c9404d
Chris <ej88ej@gmail.com>
2018-07-21 14:24:14 +0900

    style header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,12 @@
 <template>
-  <nav>
-    <router-link to="/">Trelno</router-link>
-    <a v-if="isAuthenicated" href="" @click.prevent="logout">Logout</a>
-    <router-link v-else to="/login">Login</router-link>
+  <nav class="header">
+    <div class="header-logo">
+      <router-link to="/">Trelno</router-link>
+    </div>
+    <div class="header-user">
+      <a v-if="isAuthenicated" href="" @click.prevent="logout">Logout</a>
+      <router-link v-else to="/login">Login</router-link>
+    </div>
   </nav>
 </template>
 
@@ -21,3 +25,44 @@ export default {
   }
 }
 </script>
+
+<style>
+.header {
+  background-color: rgba(0,0,0,.15);
+  height: 32px;
+  padding: 4px;
+}
+.header a {
+  display: block;
+  height: 30px;
+  line-height: 30px;
+  text-decoration: none;
+  color:  rgba(255,255,255,.5);
+}
+.header-logo {
+  position: absolute;
+  left: 50%;
+  margin-left: -30px;
+  text-align: center;
+  font-weight: bolder;
+  font-size: 24px;
+}
+.header-logo a:hover,
+.header-logo a:focus {
+  color: rgba(255,255,255,.9);
+}
+.header-user {
+  position: absolute;
+  right: 15px;
+}
+.header-user a {
+  border-radius: 2px;
+  padding: 0 10px;
+  background-color: rgba(255,255,255, .5);
+  color: white;
+}
+.header-user a:hover,
+.header-user a:focus {
+  background-color: rgba(255,255,255, .3);
+}
+</style>

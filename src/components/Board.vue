3156f5083362a9c556bb83037be4f0845532c189
김정환 <ej88ej@gmail.com>
2018-08-04 17:33:20 +0900

    보드, 카드 컴포넌트의 데이터 패치 방법

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     Board 
-    <div>bid: {{bid}}</div>
-    <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
-    <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
-    <hr />
-    <router-view></router-view>
+    <div v-if="loading">loading board...</div>
+    <div v-else>
+      <div>bid: {{bid}}</div>
+      <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+      <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+      <hr />
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -13,11 +16,21 @@
 export default {
   data() {
     return {
-      bid: 0
+      bid: 0,
+      loading: false
     }
   },
   created() {
-    this.bid = this.$route.params.bid
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      setTimeout(() => {
+        this.bid = this.$route.params.bid
+        this.loading = false
+      }, 500)
+    }
   }
 }
 </script>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     Card
-    <div>cid: {{cid}}</div>
+    <div v-if="loading">loading card ...</div>
+    <div v-else>cid: {{cid}}</div>
   </div>
 </template>
 
@@ -9,17 +10,25 @@
 export default {
   data() {
     return {
-      cid: 0
+      cid: 0,
+      loading: false
     }
   },
   watch: {
-    '$route'() {
-      this.cid = this.$route.params.cid
+    '$route': {
+      handler: 'fetchData',
+      immediate: true
     }
   },
-  created() {
-    this.cid = this.$route.params.cid
-  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      setTimeout(() => {
+        this.cid = this.$route.params.cid
+        this.loading = false
+      }, 500)
+    }
+  }
 }
 </script>
 

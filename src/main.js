8f5c606f19fa5c10c58221b277ba21a26e350a8b
김정환 <ej88ej@gmail.com>
2018-08-04 11:29:41 +0900

    직접 라우팅 설정
    reder 함수의 경우 arrow func을 쓰면 렉시컬 스코프에 의해 this가 뷰
    객체가 아니게 된다. 따라서 객체 메소드를 사용해야 함

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const Login = { template: '<div>Login Page</div>' }
+const NotFound = { template: '<div>Page not found</div>' }
+
+const routes = {
+  '/': App,
+  '/login': Login
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  computed: {
+    VueComponent() {
+      return routes[window.location.pathname] || NotFound
+    }
+  },
+  render(h) {
+    return h(this.VueComponent)
+  }
 })

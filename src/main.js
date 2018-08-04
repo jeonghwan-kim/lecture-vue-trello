7ed8cd63ce04150b17e059ffbf44d80697389934
김정환 <ej88ej@gmail.com>
2018-08-04 12:44:02 +0900

    vue-router를 이용해서 구현
    기존 라우팅을 vue-router로 구현한다

--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 const Login = { template: '<div>Login Page</div>' }
 const NotFound = { template: '<div>Page not found</div>' }
 
-const routes = {
-  '/': App,
-  '/login': Login
-}
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {path: '/', component: App},
+    {path: '/login', component: Login},
+    {path: '*', component: NotFound}
+  ]
+})
 
 new Vue({
   el: '#app',
-  computed: {
-    VueComponent() {
-      return routes[window.location.pathname] || NotFound
-    }
-  },
-  render(h) {
-    return h(this.VueComponent)
-  }
+  router,
+  render: h => h({template: '<router-view></router-view>'})
 })

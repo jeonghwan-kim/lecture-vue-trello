547f1e9c3456344e52eda46f5e1d4a9168dcc5b9
김정환 <ej88ej@gmail.com>
2018-08-04 10:56:08 +0900

    vue-cli로 코드 스캐폴딩 작성
    vue init webpack-simple

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    여기서부터 코드를 시작합니다!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>

acd695a5c2715a39a245288e08b9516de3953a07
Chris <ej88ej@gmail.com>
2018-08-05 15:33:49 +0900

    GET /boards 연동

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,18 +4,11 @@
     <div>
       Board List: 
       <div v-if="loading">Loading...</div>
-      <div v-else>Api result: 
-        <pre>{{apiRes}}</pre>
-        <pre>{{error}}</pre>
+      <div v-else>
+        <div v-for="b in boards" :key=b.id>
+          <pre>{{b}}</pre>
+        </div>
       </div>
-      <ul>
-        <li>
-          <router-link to="/b/1">Board 1</router-link>
-        </li>
-        <li>
-          <router-link to="/b/2">Board 2</router-link>
-        </li>
-      </ul>
     </div>
   </div>
 </template>
@@ -27,7 +20,7 @@ export default {
   data() {
     return {
       loading: false,
-      apiRes: '',
+      boards: '',
       error: ''
     }
   },
@@ -37,12 +30,12 @@ export default {
   methods: {
     fetchData() {
       this.loading = true
-      axios.get('http://localhost:3000/health')
+      axios.get('http://localhost:3000/boards')
         .then(res => {
-          this.apiRes = res.data
+          this.boards = res.data
         })
         .catch(res => {
-          this.error = res.response.data
+          this.$router.replace('/login')
         })
         .finally(_=> {
           this.loading = false

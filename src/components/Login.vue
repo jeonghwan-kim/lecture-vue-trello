6a57a8f122509faf9e3522c599117ce634e91fdd
Chris <ej88ej@gmail.com>
2018-08-05 17:24:44 +0900

    Login 컴포넌트 UI 개발

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,53 @@
 <template>
-  <div>
-    Login
+  <div class="login">
+    <h2>Log in to Trello</h2>
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label for="email">Email</label>
+        <input class="form-control" type="text" name="email" 
+          v-model="email" autofocus placeholder="e.g., test@test.com" />
+      </div>
+      <div>
+        <label for="password">Passwrod</label>
+        <input class="form-control" type="password" 
+          v-model="password" placeholder="123123" />
+      </div>
+      <button  class="btn" :class="{'btn-success': !invalidForm}" type="submit" 
+        :disabled="invalidForm">Log In</button>
+    </form>
+    <p class="error" v-if="error">{{error}}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: '',
+    }
+  },
+  computed: {
+    invalidForm() {
+      return !this.email || !this.password
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.email, this.password)
+    }
+  }
 }
 </script>
 
 <style>
+.login {
+  width: 400px;
+  margin: 0 auto; 
+}
+.error {
+  color: #f00;
+}
 
 </style>

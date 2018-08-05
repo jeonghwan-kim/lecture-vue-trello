2590135c890ee0bed33fc3401c8c8e57ffbf9540
Chris <ej88ej@gmail.com>
2018-08-05 17:23:58 +0900

    App 공통 스타일 추가

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,51 @@ export default {
 </script>
 
 <style>
+html, body, #app {
+  height: 100%;
+  margin: 0px;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+}
+.container {
+  flex-grow: 1;
+  position: relative;
+}
+.btn {
+  border-radius: 3px;
+  padding: 6px 8px;
+  background-color: #e2e4e6;
+  border: none;
+  display: inline-block;
+  color: #fff;
+  font-size: 14px;
+  line-height: 20px;  
+  font-weight: 700;
+  cursor: pointer;
+}
+.btn-success {
+  background-color: #5aac44;
+  box-shadow: 0 1px 0 #519839;
+}
+.form-control {
+  width: 100%;
+  box-sizing: border-box;
+  background-color: #e2e4e6;
+  border: 1px solid #cdd2d4;
+  border-radius: 3px;
+  display: block;
+  margin-bottom: 12px;
+  padding: 6px 8px;
+  transition: background-color .3s;
+}
+input[type=text].form-control,
+input[type=password].form-control,
+textarea.form-control {
+  font-size: 14px;
+}
+.form-control:focus {
+  background-color: #fff;
+}
 </style>

5ab10e547b106a9fa4163ede6cc3e712e9741349
Chris <ej88ej@gmail.com>
2018-08-05 17:12:31 +0900

    네이게이션 가드 추가

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,34 @@ import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter)
 
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem('token')
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+  isAuth ? next() : next(loginPath)
+}
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
-    { path: '/login', component: Login },
-    { path: '/b/:bid', component: Board, children: [
-      { path: 'c/:cid', component: Card }
-    ] },
-    { path: '*', component: NotFound }
+    { 
+      path: '/', 
+      component: Home,
+      beforeEnter: requireAuth
+    },
+    { 
+      path: '/login', 
+      component: Login 
+    },
+    { 
+      path: '/b/:bid', 
+      component: Board,
+      beforeEnter: requireAuth,
+      children: [{ path: 'c/:cid', component: Card }] 
+    },
+    { 
+      path: '*', 
+      component: NotFound 
+    }
   ]
 })
 

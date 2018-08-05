fefb85b9b89e7922dbc28571e13e60c2b95f4fca
Chris <ej88ej@gmail.com>
2018-08-05 14:46:07 +0900

    XMLHttpRequest 객체 사용하여 GET /health 요청

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,8 @@
     Home
     <div>
       Board List: 
+      <div v-if="loading">Loading...</div>
+      <div v-else>Api result: <pre>{{apiRes}}</pre></div>
       <ul>
         <li>
           <router-link to="/b/1">Board 1</router-link>
@@ -17,7 +19,31 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      loading: false,
+      apiRes: ''
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+      const req = new XMLHttpRequest()
+      req.open('GET', 'http://localhost:3000/health')
+      req.send()
+      req.addEventListener('load', _=> {
+        this.loading = false
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText, 
+          response: JSON.parse(req.response)
+        }
+      })
+    }
+  }
 }
 </script>
 

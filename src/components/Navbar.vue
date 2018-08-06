2af7d03f26e3fd45329afb9712be2988004894e4
Chris <ej88ej@gmail.com>
2018-08-06 12:55:19 +0900

    Navbar style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/login">Login</router-link>
-  </div>
+  <nav class="header">
+    <div class="header-logo">
+      <router-link to="/">Home</router-link>
+    </div>
+    <div class="header-auth">
+      <router-link to="/login">Login</router-link>
+      <!-- <a>Logout</a> -->
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -11,5 +16,46 @@ export default {
 </script>
 
 <style>
-
+.header {
+  flex: none;
+  background-color: rgba(0,0,0,.15);
+  height: 32px;
+  padding: 4px;
+}
+.header a {
+  display: block;
+  height: 30px;
+  line-height: 30px;
+  text-decoration: none;
+  color: rgba(255,255,255,.5);
+}
+.header-logo {
+  position: absolute;
+  left: 50%;
+  top: 7px;
+  margin-left: -30px;
+  text-align: center;
+  font-weight: bolder;
+  font-size: 24px;
+}
+.header-logo a:hover,
+.header-logo a:focus {
+  color: rgba(255,255,255,.9);
+}
+.header-auth {
+  position: absolute;
+  right: 15px;
+  top: 5px;
+}
+.header-auth a {
+  border-radius: 2px;
+  padding: 0 10px;
+  background-color: rgba(255,255,255, .5);
+  color: white;
+  transition: all .3s;
+}
+.header-auth a:hover,
+.header-auth a:focus {
+  background-color: rgba(255,255,255, .3);
+}
 </style>

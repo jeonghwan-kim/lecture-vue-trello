b0ba747f8c431ff8b5fe1fb7b71a31888683c1d2
김정환 <ej88ej@gmail.com>
2018-08-06 20:46:39 +0900

    AddBoard 컴포넌트

---
+++ src/components/AddBoard.vue
@@ -0,0 +1,58 @@
+<template>
+  <Modal>
+    <div slot="header">
+      <h2>
+        Create new board
+        <a href="" class="modal-default-button" 
+          @click.prevent="close">&times;</a>
+      </h2>
+    </div>
+    <div slot="body">
+      <form id="add-board-form" 
+        @submit.prevent="addBoard">
+        <input class="form-control" type="text" v-model="input" ref="input">
+      </form>
+    </div>
+    <div slot="footer">
+      <button class="btn" :class="{'btn-success': valid}" type="submit" 
+        form="add-board-form" :disabled="!valid">
+        Create Board</button>
+    </div>
+  </Modal>
+</template>
+
+<script>
+import Modal from './Modal.vue'
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      input: '',
+      valid: false
+    }
+  },
+  watch: {
+    input(v) {
+      this.valid = v.trim().length > 0
+    }
+  },
+  mounted() {
+    this.$refs.input.focus()
+  },
+  methods: {
+    close() {
+      this.$emit('close')
+    },
+    addBoard() {
+      this.$emit('close')
+      this.$emit('submit', this.input)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

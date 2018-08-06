8477cc6d8955e0ad0f134a0791fd25ea1865d66d
김정환 <ej88ej@gmail.com>
2018-08-06 20:46:59 +0900

    Home에서 AddBoard 사용

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,18 +14,24 @@
         </a>
       </div>
     </div>
+    <AddBoard v-if="isAddBoard" @close="isAddBoard=false" @submit="onAddBoard"/>
   </div>
 </template>
 
 <script>
 import {board} from '../api'
+import AddBoard from './AddBoard.vue'
 
 export default {
+  components: {
+    AddBoard
+  },
   data() {
     return {
       loading: false,
       boards: [],
-      error: ''
+      error: '',
+      isAddBoard: false
     }
   },
   created() {
@@ -48,7 +54,11 @@ export default {
         })
     },
     addBoard() {
-      console.log('addBoard()')
+      this.isAddBoard = true
+    },
+    onAddBoard(title) {
+      console.log(title)
+      // todo POST /boards 호출. 여기서 잠깐!
     }
   }
 }

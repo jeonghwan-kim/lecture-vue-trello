db39ec12011752d761ba00b3871506f800737087
김정환 <ej88ej@gmail.com>
2018-08-06 20:15:30 +0900

    Home 컴포넌트 스타일

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -43,5 +43,49 @@ export default {
 </script>
 
 <style>
-
+.home-title {
+  padding: 10px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.board-list {
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.board-item {
+  width: 23%;
+  height: 100px;
+  margin: 0 2% 20px 0;
+  border-radius: 3px;
+}
+.board-item-new {
+  background-color: #ddd;
+}
+.board-item a {
+  text-decoration: none;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.board-item a:hover,
+.board-item a:focus {
+  background-color: rgba(0,0,0, .1);
+  color: #666;
+}
+.board-item-title {
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  padding: 10px;
+}
+.board-item a.new-board-btn {
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+  height: 100px;
+  width: inherit;
+  color: #888;
+  font-weight: 700;
+}
 </style>

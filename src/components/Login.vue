540cde983991a6d10c99a931b61f650eb3241901
김정환 <ej88ej@gmail.com>
2018-08-11 17:31:42 +0900

    Login 적용

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-import {auth, setAuthInHeader} from '../api'
+import {mapActions} from 'vuex'
 
 export default {
   data() {
@@ -40,11 +40,12 @@ export default {
     this.rPath = this.$route.query.rPath || '/'
   },
   methods: {
+    ...mapActions([
+      'LOGIN'
+    ]),
     onSubmit() {
-      auth.login(this.email, this.password)
+      this.LOGIN({email: this.email, password: this.password})
         .then(data => {
-          localStorage.setItem('token', data.accessToken)
-          setAuthInHeader(data.accessToken)
           this.$router.push(this.rPath)
         })
         .catch(err => {

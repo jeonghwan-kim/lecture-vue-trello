9cc1b973f9878333ce81aebaa166e3474536e0d3
Chris <ej88ej@gmail.com>
2018-08-15 12:02:00 +0900

    fetch board 관련 상태 추가 (board, SET_BOARD, FETCH_BOARD)

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,11 +6,7 @@ const actions = {
       .then(({accessToken}) => commit('LOGIN', accessToken))
   },
   ADD_BOARD (_, {title}) {
-<<<<<<< HEAD
     return api.board.create(title).then(data => data.item)
-=======
-    return api.board.create(title).then(data => data.itme)
->>>>>>> ADD_BOARD 액션 응답값 개선
   },
   FETCH_BOARDS ({commit}) {
     return api.board.fetch().then(data => {

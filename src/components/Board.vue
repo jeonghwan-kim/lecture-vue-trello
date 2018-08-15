ef62a1be1ce2e457f9336ef34ec53b307a284635
김정환 <ej88ej@gmail.com>
2018-08-15 20:39:07 +0900

    board css 클래스 추가

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -43,5 +43,78 @@ export default {
 </script>
 
 <style>
-
+.board-wrapper {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+.board {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.board-header {
+  flex: none;
+  padding: 8px 4px 8px 8px;
+  margin: 0;
+  height: 32px;
+  line-height: 32px;
+} 
+.board-header input[type=text] {
+  width: 200px;
+}
+.board-header-btn {
+  border-radius: 4px;
+  padding: 2px 10px;
+  height: 30px;
+  margin-bottom: 15px;
+  display: inline-block;
+  color: #fff;
+}
+.board-header-btn:hover,
+.board-header-btn:focus {
+  background-color: rgba(0,0,0,.15);
+  cursor: pointer;
+}
+.board-title {
+  font-weight: 700;
+  font-size: 18px;
+}
+.show-menu {
+  font-size: 14px;
+  position: absolute;
+  right: 15px;
+}
+.list-section-wrapper {
+  flex-grow: 1;
+  position: relative;
+}
+.list-section {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow-x: auto;
+  overflow-y: hidden;
+  white-space: nowrap;
+  padding: 0 10px;
+}
+.list-wrapper {
+  display: inline-block;
+  height: 100%;
+  width: 272px;
+  vertical-align: top;
+  margin-right: 5px;
+}
+.card-item.gu-transit {
+  background-color: #555 !important;
+}
+.card-item.gu-mirror {
+  opacity: 1 !important;
+  background-color: #fff !important;
+  transform: rotate(3deg) !important;
+}
 </style>

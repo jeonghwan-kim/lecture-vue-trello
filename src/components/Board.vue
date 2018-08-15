f5094fc2271bb48337805eda5418791c6330dec4
Chris <ej88ej@gmail.com>
2018-08-15 12:02:24 +0900

    fetch board api 호출 결과 출력

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-    Board 
     <div v-if="loading">loading board...</div>
     <div v-else>
-      <div>bid: {{bid}}</div>
+      <pre>{{board}}</pre>
       <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
       <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
       <hr />
@@ -13,6 +12,8 @@
 </template>
 
 <script>
+import {mapState, mapActions} from 'vuex'
+
 export default {
   data() {
     return {
@@ -20,16 +21,22 @@ export default {
       loading: false
     }
   },
+  computed: {
+    ...mapState({
+      board: 'board'
+    })
+  },
   created() {
     this.fetchData()
   },
   methods: {
+    ...mapActions([
+      'FETCH_BOARD'
+    ]),
     fetchData() {
       this.loading = true
-      setTimeout(() => {
-        this.bid = this.$route.params.bid
-        this.loading = false
-      }, 500)
+      this.FETCH_BOARD({id: this.$route.params.bid})
+        .then(() => this.loading = false)
     }
   }
 }

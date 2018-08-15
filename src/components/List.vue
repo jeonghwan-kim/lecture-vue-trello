a44007926449736de7e1fbe7015bdafec877533f
김정환 <ej88ej@gmail.com>
2018-08-15 20:51:50 +0900

    List 스타일 추가

---
+++ src/components/List.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+}
+</script>
+
+<style>
+.list {
+  background-color: #e2e4e6;
+  border-radius: 3px;
+  margin-right: 10px;
+  display: flex;
+  flex-direction: column;
+  vertical-align: top;
+  width: 100%;
+  max-height: 100%;
+}
+.list-header {
+  flex: 0 0 auto;
+  height: 30px;
+  padding: 10px 8px 8px;
+  position: relative;
+}
+.list-header-title {
+  font-size: 16px;
+  font-weight: 700;
+  padding-left: 8px;
+  line-height: 30px;
+}
+.input-title {
+  width: 90%;
+}
+.delete-list-btn {
+  position: absolute;
+  right: 10px;
+  top: 8px;
+  text-decoration: none;
+  color: #aaa;
+  font-size: 24px;
+}
+.card-list {
+  flex: 1 1 auto;
+  overflow-y: scroll;
+}
+.empty-card-item   {
+  height: 10px;
+  width: 100%;
+  background-color: rgba(0,0,0, 0);
+}
+.add-card-btn {
+  flex: 0 0 auto;
+  display: block;
+  padding: 8px 10px;
+  color: #8c8c8c;
+  text-decoration: none;
+}
+.add-card-btn:focus,
+.add-card-btn:hover {
+  background-color: rgba(0,0,0, .1);
+}
+</style>

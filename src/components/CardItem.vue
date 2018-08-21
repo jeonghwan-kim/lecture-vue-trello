cfc82f98bb11fb20b43bc54fbe1a012445ce2df4
김정환 <ej88ej@gmail.com>
2018-08-21 21:22:30 +0900

    CardItem 마크업/스타일 작업

---
+++ src/components/CardItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card-item">
+    <div>{{data.title}}</div>
+    <div class="card-item-meta" v-if="data.description">&equiv;</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data']
+}
+</script>
+
+<style>
+.card-item {
+  background-color: #fff;
+  border-radius: 3px;
+  margin: 8px;
+  padding: 6px 20px 2px 8px;
+  box-shadow: 0 1px 0 #ccc;
+  position: relative;
+}
+.card-item a {
+  text-decoration: none;
+  color: #444;
+  word-wrap: break-word;
+  white-space: normal;
+  overflow: hidden;
+  display: block;
+}
+.card-item:hover,
+.card-item:focus {
+  background-color: rgba(0,0,0, .1);
+  cursor: pointer;
+}
+.card-item-meta {
+  font-size: 26px;
+  padding: 5px 0 0 3px;
+  color: #8c8c8c;  
+}
+.delete-card-btn {
+  position: absolute;
+  right: 10px;
+  top: 4px;
+  text-decoration: none;
+  font-size: 18px;
+  color: #aaa;
+}
+</style>

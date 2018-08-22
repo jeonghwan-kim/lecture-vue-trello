e115590eba225d6351f6a4f125aa1c248aff6e70
김정환 <ej88ej@gmail.com>
2018-08-22 21:31:57 +0900

    CardItem 안에 router-link 추가

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="card-item">
-    <div>{{data.title}}</div>
-    <div class="card-item-meta" v-if="data.description">&equiv;</div>
+    <router-link :to="`/b/${boardId}/c/${data.id}`">
+      <div>{{data.title}}</div>
+      <div class="card-item-meta" v-if="data.description">&equiv;</div>
+    </router-link>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+
 export default {
-  props: ['data']
+  props: ['data'],
+  computed: {
+    ...mapState({
+      'boardId': state => state.board.id
+    })
+  }
 }
 </script>
 

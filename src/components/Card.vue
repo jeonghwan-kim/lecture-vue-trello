d1a20703f998799eca08c3e80ea3ddc43b7ef973
김정환 <ej88ej@gmail.com>
2018-08-22 21:54:31 +0900

    Card markup/css 작업

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,18 @@
 <template>
-  <Modal>
-    <div slot="body">
-      {{card}}
+  <Modal class="modal-card">
+    <div slot="header" class="modal-card-header">
+      <div class="modal-card-header-title">
+        <input class="form-control" type="text" :value="card.title" readonly>
+      </div>
+      <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
     </div>
+    <div slot="body">
+      <h3>Description</h3>
+      <textarea  class="form-control" cols="30" rows="3" placeholder="Add a more detailed description..."
+        readonly
+        v-model="card.description"></textarea>
+    </div>
+    <div slot="footer"></div>
   </Modal>
 </template>
 
@@ -14,7 +24,8 @@ export default {
   components: {Modal},
   computed: {
     ...mapState({
-      card: 'card'
+      card: 'card',
+      board: 'board'
     })
   },
   created() {
@@ -24,11 +35,38 @@ export default {
   methods: {
     ...mapActions([
       'FETCH_CARD'
-    ])    
+    ]),
+    onClose() {
+      this.$router.push(`/b/${this.board.id}`)
+    },
+    onBlurTitle() {
+      this.toggleTitle = false
+      const title = this.$refs.inputTitle.value.trim()
+      if (!title) return 
+      this.UPDATE_CARD({id: this.card.id, title})
+        .then(()=> this.fetchCard())
+    }
   }
 }
 </script>
 
 <style>
-
+.modal-card .modal-container {
+  min-width: 300px;
+  max-width: 800px;
+  width: 60%;
+}
+.modal-card-header-title {
+  padding-right: 30px;  
+}
+.modal-close-btn {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  font-size: 24px;
+  text-decoration: none;
+}
+.modal-card-header {
+  position: relative;
+}
 </style>

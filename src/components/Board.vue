9196884bb149c620756d8aad7ac81b778cda2fe3
Chris <ej88ej@gmail.com>
2018-09-03 11:49:59 +0900

    카드 이동 리팩토링: dragger 모듈 추가

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -21,8 +21,7 @@
 <script>
 import {mapState, mapActions} from 'vuex'
 import List from './List.vue'
-import dragula from 'dragula'
-import 'dragula/dist/dragula.css'
+import dragger from '../utils/dragger'
 
 export default {
   components: { List },
@@ -30,7 +29,7 @@ export default {
     return {
       bid: 0,
       loading: false,
-      dragulaCards: null
+      cDragger: null
     }
   },
   computed: {
@@ -42,36 +41,7 @@ export default {
     this.fetchData()
   },
   updated() {
-    if (this.dragulaCards) this.dragulaCards.destroy()
-    
-    this.dragulaCards = dragula([
-      ...Array.from(this.$el.querySelectorAll('.card-list'))
-    ]).on('drop', (el, wrapper, target, silblings) => {
-      const targetCard = {
-        id: el.dataset.cardId * 1, 
-        pos: 65535,
-      }
-      let prevCard = null
-      let nextCard = null
-      Array.from(wrapper.querySelectorAll('.card-item'))
-        .forEach((el, idx, arr) => {
-          const cardId = el.dataset.cardId * 1
-          if (targetCard.id === cardId) {
-            prevCard = idx > 0 ? {
-              id: arr[idx - 1].dataset.cardId * 1,
-              pos: arr[idx - 1].dataset.cardPos * 1,
-            } : null
-            nextCard = idx < arr.length - 1 ? {
-              id: arr[idx + 1].dataset.cardId * 1,
-              pos: arr[idx + 1].dataset.cardPos * 1,
-            } : null
-          }
-        })
-      if (!prevCard && nextCard) targetCard.pos = nextCard.pos / 2
-      else if (!nextCard && prevCard) targetCard.pos = prevCard.pos * 2
-      else if (nextCard && prevCard) targetCard.pos = (prevCard.pos + nextCard.pos) / 2
-      this.UPDATE_CARD(targetCard)
-    })
+    this.setCardDragabble()
   },
   methods: {
     ...mapActions([
@@ -82,6 +52,29 @@ export default {
       this.loading = true
       this.FETCH_BOARD({id: this.$route.params.bid})
         .then(() => this.loading = false)
+    },
+    setCardDragabble() {
+      if (this.cDragger) this.cDragger.destroy()
+    
+      this.cDragger = dragger.init(Array.from(this.$el.querySelectorAll('.card-list')))
+      this.cDragger.on('drop', (el, wrapper, target, silblings) => {
+        const targetCard = {
+          id: el.dataset.cardId * 1, 
+          listId: wrapper.dataset.listId * 1,
+          pos: 65535,
+        }
+        const {prev, next} = dragger.sibling({
+          el, 
+          wrapper, 
+          candidates: Array.from(wrapper.querySelectorAll('.card-item')), 
+          type: 'card'
+        })
+        
+        if (!prev && next) targetCard.pos = next.pos / 2
+        else if (!next && prev) targetCard.pos = prev.pos * 2
+        else if (next && prev) targetCard.pos = (prev.pos + next.pos) / 2
+        this.UPDATE_CARD(targetCard)
+      })
     }
   }
 }

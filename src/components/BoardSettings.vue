8089e9fc414147fba36df9fb9e87a5dcb6d5749f
Chris <ej88ej@gmail.com>
2018-09-03 14:05:50 +0900

    BoardSettings에 보드 삭제 기능 구현

--- src/components/BoardSettings.vue
+++ src/components/BoardSettings.vue
@@ -5,21 +5,35 @@
       <a class="header-close-btn" href="" @click.prevent="onClose">&times;</a>
     </div>
     <ul class="menu-list">
-      <li>Menu 1</li>
+      <li><a href="" @click.prevent="onDeleteBoard">Delete Board</a></li>
     </ul>
   </div>
 </template>
 
 <script>
-import {mapMutations} from 'vuex'
+import {mapState, mapMutations, mapActions} from 'vuex'
 
 export default {
+  computed: {
+    ...mapState({
+      board: 'board'
+    })
+  },
   methods: {
     ...mapMutations([
       'SET_IS_SHOW_BOARD_SETTINGS'
     ]),
+    ...mapActions([
+      'DELETE_BOARD'
+    ]),
     onClose() {
       this.SET_IS_SHOW_BOARD_SETTINGS(false)
+    },
+    onDeleteBoard() {
+      if (!confirm(`Delete ${this.board.title} Board?`)) return 
+      this.DELETE_BOARD({id: this.board.id})
+      .then(_=> this.SET_IS_SHOW_BOARD_SETTINGS(false))
+      .then(_=> this.$router.push('/'))
     }
   }
 }

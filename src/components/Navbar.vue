c8405290336ed8880fa29f9ebec0ee1d6b09a6c7
Chris <ej88ej@gmail.com>
2018-09-03 13:42:00 +0900

    Navbar에서 테마 세팅

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,13 +11,23 @@
 </template>
 
 <script>
-import {mapGetters, mapMutations} from 'vuex'
+import {mapState, mapGetters, mapMutations} from 'vuex'
 
 export default {
   computed: {
+    ...mapState({
+      navbarColor: 'navbarColor',
+      bodyColor: 'bodyColor',
+    }),
     ...mapGetters([
       'isAuth'
     ]),
+  },
+  watch: {
+    'bodyColor': 'updateTheme'
+  },
+  mounted() {
+    this.updateTheme()
   },
   methods: {
     ...mapMutations([
@@ -26,6 +36,13 @@ export default {
     logout() {
       this.LOGOUT()
       this.$router.push('/login')
+    },
+    updateTheme() {
+      this.$el.style.backgroundColor = this.navbarColor
+      const body = document.querySelector('body')
+      const container = document.querySelector('.container')
+      if (body) body.style.backgroundColor = this.bodyColor
+      if (container) container.style.backgroundColor = this.bodyColor
     }
   }
 }

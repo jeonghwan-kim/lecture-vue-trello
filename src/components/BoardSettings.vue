e91ffa1198516374c544ce7b9e154858d2729e3b
Chris <ej88ej@gmail.com>
2018-09-03 14:31:51 +0900

    보드 색성 수정 기능 구현

--- src/components/BoardSettings.vue
+++ src/components/BoardSettings.vue
@@ -6,6 +6,13 @@
     </div>
     <ul class="menu-list">
       <li><a href="" @click.prevent="onDeleteBoard">Delete Board</a></li>
+      <li>Change Background</li>
+      <div class="color-picker">
+        <a href="" data-value="rgb(0, 121, 191)" @click.prevent="onChangeTheme"></a>
+        <a href="" data-value="rgb(210, 144, 52)" @click.prevent="onChangeTheme"></a>
+        <a href="" data-value="rgb(81, 152, 57)" @click.prevent="onChangeTheme"></a>
+        <a href="" data-value="rgb(176, 70, 50)" @click.prevent="onChangeTheme"></a>
+      </div>
     </ul>
   </div>
 </template>
@@ -19,12 +26,19 @@ export default {
       board: 'board'
     })
   },
+  mounted() {
+    Array.from(this.$el.querySelectorAll('.color-picker a')).forEach(el => {
+      el.style.backgroundColor = el.dataset.value
+    })
+  },
   methods: {
     ...mapMutations([
-      'SET_IS_SHOW_BOARD_SETTINGS'
+      'SET_IS_SHOW_BOARD_SETTINGS',
+      'SET_THEME',
     ]),
     ...mapActions([
-      'DELETE_BOARD'
+      'DELETE_BOARD',
+      'UPDATE_BOARD',
     ]),
     onClose() {
       this.SET_IS_SHOW_BOARD_SETTINGS(false)
@@ -34,6 +48,12 @@ export default {
       this.DELETE_BOARD({id: this.board.id})
       .then(_=> this.SET_IS_SHOW_BOARD_SETTINGS(false))
       .then(_=> this.$router.push('/'))
+    },
+    onChangeTheme(el) {
+      const id = this.board.id
+      const bgColor = el.target.dataset.value
+      this.UPDATE_BOARD({id, bgColor})
+        .then(_=> this.SET_THEME(bgColor))
     }
   }
 }

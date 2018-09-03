6114a8b48337ac9d6a668800c865ae00bcd83207
Chris <ej88ej@gmail.com>
2018-09-03 14:43:12 +0900

    보드 타이틀 수정

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,7 +3,9 @@
     <div class="board-wrapper">
       <div class="board">
         <div class="board-header">
-          <span class="board-title">{{board.title}}</span>
+          <input class="form-control" v-if="isEditTitle" type="text" v-model="inputTitle" 
+            ref="inputTitle" @keyup.enter="onSubmitTitle" @blur="onSubmitTitle">
+          <span v-else class="board-title" @click="onClickTitle">{{board.title}}</span>
           <a class="board-header-btn show-menu" href="" @click.prevent="onShowSettings" >... Show Menu</a>
         </div>
         <div class="list-section-wrapper">
@@ -35,7 +37,9 @@ export default {
     return {
       bid: 0,
       loading: false,
-      cDragger: null
+      cDragger: null,
+      isEditTitle: false,
+      inputTitle: ''
     }
   },
   computed: {
@@ -46,6 +50,7 @@ export default {
   },
   created() {
     this.fetchData().then(() => {
+      this.inputTitle = this.board.title
       this.SET_THEME(this.board.bgColor)
     })
     this.SET_IS_SHOW_BOARD_SETTINGS(false)
@@ -60,7 +65,8 @@ export default {
     ]),
     ...mapActions([
       'FETCH_BOARD',
-      'UPDATE_CARD'
+      'UPDATE_CARD',
+      'UPDATE_BOARD',
     ]),
     fetchData() {
       this.loading = true
@@ -70,6 +76,22 @@ export default {
     onShowSettings() {
       this.SET_IS_SHOW_BOARD_SETTINGS(true)
     },
+    onClickTitle() {
+      this.isEditTitle = true
+      this.$nextTick(_=> this.$refs.inputTitle.focus())
+    },
+    onSubmitTitle() {
+      this.isEditTitle = false
+
+      this.inputTitle = this.inputTitle.trim()
+      if (!this.inputTitle) return 
+      
+      const id = this.board.id
+      const title = this.inputTitle
+      if (title === this.board.title) return 
+      
+      this.UPDATE_BOARD({ id, title })
+    },  
     setCardDragabble() {
       if (this.cDragger) this.cDragger.destroy()
     

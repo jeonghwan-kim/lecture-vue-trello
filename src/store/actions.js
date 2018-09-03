bc7503932c1b52f9e538c0a09e92c2ea301747f4
Chris <ej88ej@gmail.com>
2018-09-03 09:28:25 +0900

    action 응답값 promise로 변경

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,18 +9,18 @@ const actions = {
     return api.board.create(title).then(data => data.item)
   },
   FETCH_BOARDS ({commit}) {
-    api.board.fetch().then(data => {
+    return api.board.fetch().then(data => {
       commit('SET_BOARDS', data.list)
     })
   },
   FETCH_BOARD ({commit}, {id}) {
-    api.board.fetch(id).then(data => {
+    return api.board.fetch(id).then(data => {
       commit('SET_BOARD', data.item)
     })
   },
 
   ADD_CARD({dispatch, state}, {title, listId, pos}) {
-    api.card.create(title, listId, pos)
+    return api.card.create(title, listId, pos)
       .then(_ => dispatch('FETCH_BOARD', {id: state.board.id}))
   },
 }  

b18cae9382038dc5a96d350c1dcf1b11f7f093f0
김정환 <6pack@madup.com>
2018-09-25 14:58:06 +0900

    list 삭제 api/action 등록

--- src/api/index.js
+++ src/api/index.js
@@ -46,6 +46,9 @@ export const list = {
   update(id, payload) {
     return request('put', `/lists/${id}`, payload)
   },
+  destroy(id) {
+    return request('delete', `/lists/${id}`)
+  },
 }
 
 export const card = {
--- src/store/actions.js
+++ src/store/actions.js
@@ -33,7 +33,11 @@ const actions = {
   UPDATE_LIST ({ state, dispatch }, {id, pos, title}) {
     return api.list.update(id, { pos, title })
       .then(_ => dispatch('FETCH_BOARD', {id: state.board.id}))
-  },  
+  },
+  DELETE_LIST ({ state, dispatch }, {id}) {
+    return api.list.destroy(id)
+      .then(_ => dispatch('FETCH_BOARD', {id: state.board.id}))
+  },
 
   ADD_CARD({dispatch, state}, {title, listId, pos}) {
     return api.card.create(title, listId, pos)
